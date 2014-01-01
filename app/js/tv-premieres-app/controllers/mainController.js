4fa070d966e5cc0f6513929e98ceb19453e98b80
yoh1902 <sekmous81@gmail.com>
2014-01-01 16:56:35 +0100

    Ordering

--- app/js/tv-premieres-app/controllers/mainController.js
+++ app/js/tv-premieres-app/controllers/mainController.js
@@ -3,6 +3,12 @@ app.controller("mainController", function($scope, $http){
     $scope.apiKey = "9fc4c21439ac8d6f0b65462b4d8e6082";
 	$scope.results = [];
 	$scope.filterText = null;
+	$scope.availableGenres = [];
+    $scope.genreFilter = null;
+    $scope.orderFields = ["Air Date", "Rating"];
+$scope.orderDirections = ["Descending", "Ascending"];
+$scope.orderField = "Air Date"; //Default order field
+$scope.orderReverse = false;
     $scope.init = function() {
         //API requires a start date
         var today = new Date();
@@ -19,13 +25,57 @@ app.controller("mainController", function($scope, $http){
                     //Create a date string from the timestamp so we can filter on it based on user text input
                     tvshow.date = date; //Attach the full date to each episode
                     $scope.results.push(tvshow);
+                    //Loop through each genre for this episode
+                    angular.forEach(tvshow.show.genres, function(genre, index){
+                        //Only add to the availableGenres array if it doesn't already exist
+                        var exists = false;
+                        angular.forEach($scope.availableGenres, function(avGenre, index){
+                            if (avGenre == genre) {
+                                exists = true;
+                            }
+                        });
+                        if (exists === false) {
+                            $scope.availableGenres.push(genre);
+                        }
+                    });
                 });
             });
         }).error(function(error) {
  
         });
     };
+    $scope.setGenreFilter = function(genre) {
+    $scope.genreFilter = genre;
+}
+$scope.customOrder = function(tvshow) {
+    switch ($scope.orderField) {
+        case "Air Date":
+            return tvshow.episode.first_aired;
+            break;
+        case "Rating":
+            return tvshow.episode.ratings.percentage;
+            break;
+    }
+};
 });
 
+
+app.filter('isGenre', function() {
+    return function(input, genre) {
+        if (typeof genre == 'undefined' || genre == null) {
+            return input;
+        } else {
+            var out = [];
+            for (var a = 0; a < input.length; a++){
+                for (var b = 0; b < input[a].show.genres.length; b++){
+                    if(input[a].show.genres[b] == genre) {
+                        out.push(input[a]);
+                    }
+                }
+            }
+            return out;
+        }
+    };
+});
 
 
